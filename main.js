96de166de6c663fa070bcb4deee5dbddb82e6b78
sajanabkr <112645685+sajanabkr@users.noreply.github.com>
2022-09-04 17:01:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,114 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+	  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+    {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://i.ibb.co/9pFVs1K/img-1.jpg",
+	Link: "#",
+  },
+];
 let shop = document.getElementById("shop");
-
-let basket = JSON.parse(localStorage.getItem("data")) || [];
-
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
-      let { id, name, price, desc, img, Link } = x;
-      let search = basket.find((x) => x.id === id) || [];
+      let { id, name, price, desc, img, Link } = x;   
       return `
     <div id=product-id-${id} class="item">
         <img width="220" src=${img} alt="">
